93ca15a860a0f1b2ec3cfe36182e37f235fc24ab
unknown <memeyun2000@126.com>
2021-05-18 06:15:16 +0800

    jetty resource handler

---
+++ java/jetty/demo-20210517-resource-handler/src/main/java/com/sec/jetty/App.java
@@ -0,0 +1,29 @@
+package com.sec.jetty;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.Resource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setBaseResource(Resource.newResource("target/classes/"));
+        resourceHandler.setDirectoriesListed(false);
+
+        server.setHandler(resourceHandler);
+        server.start();
+    }
+}
