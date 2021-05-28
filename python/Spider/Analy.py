42d01333231434af6188881ce16b3cc97e36b9ad
guo <memeyun2000@126.com>
2021-05-28 18:21:43 +0800

    add  analy demo

---
+++ python/Spider/Analy.py
@@ -0,0 +1,28 @@
+import sqlite3
+import matplotlib.pyplot as plt
+import numpy as np
+
+conn = sqlite3.connect("/apps/demo/Demo/python/Spider/resoureces/sqlite3.db")
+cur = conn.cursor()
+# whereStr = " and fund_dt >= '2021-03-01' and fund_dt <= '2021-07-01' "
+whereStr = ""
+cur.execute("select fund_dt , amt from fund_worth where fund_id = '001792' " + whereStr)
+
+fundList = cur.fetchall()
+
+dateList = []
+acList = []
+for fund in fundList:
+    dateList.append(fund[0])
+    acList.append(fund[1])
+
+
+
+print(dateList)
+print(acList)
+
+
+plt.plot(dateList,acList)
+plt.show()
+
+
